0116e669f0551b927d0a7ff11dd9b30137dd236e
Jun <tjrwns0012@naver.com>
2026-02-25 11:29:54 +0900

    반복문, 배열 추가

--- main.cpp
+++ main.cpp
@@ -1,47 +1,16 @@
-﻿// 2026.02.25 김석준
-#include<iostream>
+﻿#include<iostream>
 
 using namespace std;
 
-int Sum(int a, int b) // 더하기
-{
-	return a + b;
-}
-
-int Difference(int a, int b) // 빼기
-{
-	return a - b;
-}
-
-int Product(int a, int b) // 곱하기
-{
-	return a * b;
-}
-
-int Quotient(int a, int b) // 나누기
-{
-	return a / b;
-}
-
-int Remainder(int a, int b) // 나머지
-{
-	return a % b;
-}
-
 int main()
 {
-	int Number1;
-	int Number2;
+	int Array[10] = {0,};
 
-	cin >> Number1;
-	cin >> Number2;
-
-	cout << Sum(Number1, Number2) << endl;
-	cout << Difference(Number1, Number2) << endl;
-	cout << Product(Number1, Number2) << endl;
-	cout << Quotient(Number1, Number2) << endl;
-	cout << Remainder(Number1, Number2) << endl;
-	cout << "집" << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		Array[i] = i + 1;
+		cout << Array[i] << endl;
+	}
 
 	return 0;
 }
